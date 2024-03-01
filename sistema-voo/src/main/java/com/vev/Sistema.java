41b247975fb31cc068ea00aabb8041f844a1b120
Pedro Serey <serey.pedro@gmail.com>
2024-03-01 20:27:24 -0300

    add: base maven project

---
+++ sistema-voo/src/main/java/com/vev/Sistema.java
@@ -0,0 +1,11 @@
+package com.vev;
+
+/**
+ * Classe principal do sistema de gerenciamento reservas de voos.
+ *
+ */
+public class Sistema {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
