397f2a793351057f5c624c068e448e39fa28ee79
Pedro Serey <serey.pedro@gmail.com>
2024-03-02 12:35:09 -0300

    add: UserServiceTest (red)

---
+++ sistema-voo/src/test/java/com/vev/UserServiceTest.java
@@ -0,0 +1,32 @@
+package com.vev;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+    private UserService us;
+
+    @BeforeEach
+    void setUp() {
+        this.us = new UserService();
+    }
+
+    @Test
+    void testCriaUser() {
+        String nome = "Usuário Teste";
+        String telefone = "82988887777";
+        String cpf = "44455566677";
+
+        us.criaUser(nome, telefone, cpf);
+
+        User user = this.us.getUserByCpf(cpf);
+
+        assertEquals(user.getNome(), nome);
+        assertEquals(user.getTelefone(), telefone);
+        assertEquals(user.getCpf(), cpf);
+        assertTrue(user.getReservas().isEmpty());
+    }
+}
