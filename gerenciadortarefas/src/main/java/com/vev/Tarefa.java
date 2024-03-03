36faf25fa646a58de72e03570aecb5731b849242
lorennavictoria <lorenna.lucena@ccc.ufcg.edu.br>
2024-03-03 00:27:30 -0300

    add documentação

--- gerenciadortarefas/src/main/java/com/vev/Tarefa.java
+++ gerenciadortarefas/src/main/java/com/vev/Tarefa.java
@@ -1,5 +1,8 @@
 package com.vev;
 
+/**
+ * A classe Tarefa representa uma tarefa a ser realizada, contendo informações como título, descrição, data de vencimento e prioridade.
+ */
 public class Tarefa {
     
     private String titulo;
@@ -7,6 +10,13 @@ public class Tarefa {
     private String dataVencimento;
     private Prioridade prioridade;
 
+    /**
+     * Construtor da classe Tarefa.
+     * @param titulo O título da tarefa.
+     * @param descricao A descrição da tarefa.
+     * @param dataVencimento A data de vencimento da tarefa no formato de string.
+     * @param prioridade A prioridade da tarefa.
+     */
     public Tarefa(String titulo, String descricao, String dataVencimento, Prioridade prioridade) {
         this.titulo = titulo;
         this.descricao = descricao;
@@ -14,38 +24,74 @@ public class Tarefa {
         this.prioridade = prioridade;
     }
 
+    /**
+     * Define um novo título para a tarefa.
+     * @param novoTitulo O novo título da tarefa.
+     */
     public void setTitulo(String novoTitulo){
         this.titulo = novoTitulo;
     }
 
+    /**
+     * Define uma nova descrição para a tarefa.
+     * @param novaDescricao A nova descrição da tarefa.
+     */
     public void setDescricao(String novaDescricao){
         this.descricao = novaDescricao;
     }
 
+    /**
+     * Define uma nova data de vencimento para a tarefa.
+     * @param novaDataVencimento A nova data de vencimento da tarefa no formato de string.
+     */
     public void setDataVencimento(String novaDataVencimento){
         this.dataVencimento = novaDataVencimento;
     }
-    
+
+     /**
+     * Define uma nova prioridade para a tarefa.
+     * @param novaPrioridade A nova prioridade da tarefa.
+     */
     public void setPrioridade(Prioridade novaPrioridade){
         this.prioridade = novaPrioridade;
     }
 
+    /**
+     * Obtém o título da tarefa.
+     * @return O título da tarefa.
+     */
     public String getTitulo() {
         return this.titulo;
     }
 
+    /**
+     * Obtém a descrição da tarefa.
+     * @return A descrição da tarefa.
+     */
     public String getDescricao() {
         return this.descricao;
     }
 
+    /**
+     * Obtém a data de vencimento da tarefa.
+     * @return A data de vencimento da tarefa no formato de string.
+     */
     public String getDataVencimento() {
         return this.dataVencimento;
     }
 
+    /**
+     * Obtém a prioridade da tarefa.
+     * @return A prioridade da tarefa.
+     */
     public Prioridade getprioridade() {
         return this.prioridade;
     }
-
+    
+    /**
+     * Retorna uma representação em string da tarefa.
+     * @return Uma representação em string da tarefa contendo título, descrição, data de vencimento e prioridade.
+     */
     public String toString(){
         return "Título: " + this.titulo 
             + " \n Descrição: " + this.descricao 
