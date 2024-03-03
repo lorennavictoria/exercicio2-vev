6389841dff1f31b5d67d8e24a9f21be92674ac7f
lorennavictoria <lorenna.lucena@ccc.ufcg.edu.br>
2024-03-02 22:06:25 -0300

    add versão inicial do teste da função de listagem de tarefas

--- gerenciadortarefas/src/test/java/com/vev/GerenciadorTarefasTest.java
+++ gerenciadortarefas/src/test/java/com/vev/GerenciadorTarefasTest.java
@@ -41,5 +41,28 @@ public class GerenciadorTarefasTest {
         assertTrue(gerenciadorTarefas.tarefas.size(), 0);
     }    
 
+    @Test
+    public void listaTarefasTest(){
+        Tarefa tarefa1 = new Tarefa("Estudar Java", "Preparar para a prova", "10/02/2023", Priority.ALTA);
+        Tarefa tarefa2 = new Tarefa("Fazer exercícios", "Treino na academia", "08/01/2024", Priority.BAIXA);
+        gerenciadorTarefas.criaTarefa(tarefa1);
+        gerenciadorTarefas.criaTarefa(tarefa2);
+
+        // Ordenada por prioridade
+        String tarefas = taskManager.listaTarefas("prioridade");
+        String tarefasEsperadas = "Título: Estudar Java \n Descrição: Preparar para a prova \n Data de vencimento:10/12/2023 \n Prioridade: Alta" +
+                            "Título: Fazer exercícios \n Descrição: Treino na academia \n Data de vencimento:08/01/2024 \n Prioridade: Baixa";
+
+        assertEquals(tarefas, tarefasEsperadas);
+        
+        // Ordenada por data de vencimento
+        tarefas = taskManager.listaTarefas("prioridade");
+        tarefasEsperadas = "Título: Estudar Java \n Descrição: Preparar para a prova \n Data de vencimento:10/12/2023 \n Prioridade: Baixa" +
+                            "Título: Fazer exercícios \n Descrição: Treino na academia \n Data de vencimento:08/01/2024 \n Prioridade: Alta";
+
+
+        assertEquals(tarefas, tarefasEsperadas);
+    }
+
 
 }
